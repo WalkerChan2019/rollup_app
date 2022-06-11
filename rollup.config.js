d8d4f0fbd1fa213a8e28d0bf0bb1046ab85da6dc
walkerChan <walkerchan2019@163.com>
2022-06-11 17:22:39 +0800

    rollup编译+jest单元测试

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+// const vuePlugin = require("../../rollup-plugin-vue/index");
+import babel from "@rollup/plugin-babel";
+import vue from "rollup-plugin-vue";
+import resolve from "@rollup/plugin-node-resolve";
+
+const extensions = [".js", ".ts", ".tsx"];
+const umd = {
+  input: "src/entry.ts",
+  output: {
+    file: "dist/smartyui.umd.js", // umd
+    name: "SmartyUI",
+    format: "umd",
+    exports: "named",
+    globals: {
+      vue: "Vue",
+    },
+  },
+  extensions,
+  external: ["vue"],
+  plugins: [
+    vue({
+      css: true,
+    }),
+
+    babel({
+      exclude: "node_modules/**",
+      extensions,
+      plugins: [
+        [
+          "module-resolver",
+          {
+            extensions,
+            root: ["./src"],
+          },
+        ],
+      ],
+    }),
+    // 默认模块扩展名
+    resolve({
+      extensions,
+      modulesOnly: true,
+      preferredBuiltins: false,
+    }),
+  ],
+};
+
+export default [umd];
